7c50890e76cdc75b74001ef9b21a28342f36093c
PawanGu <66518683+PawanGu@users.noreply.github.com>
2025-08-19 12:35:43 +0530

    Create Black_Scholes.py

---
+++ Black_Scholes.py
@@ -0,0 +1,10 @@
+from scipy.stats import norm
+import numpy as np
+
+def black_scholes_call(S, K, T, r, sigma):
+    if T <= 0:
+        return max(S - K, 0)
+    d1 = (np.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    call_price = S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+    return call_price
